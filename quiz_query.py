69abe6bcd21dffd86b3ef8c35189eb219f4994d5
TheSriramM <161264109+TheSriramM@users.noreply.github.com>
2025-05-12 21:13:21 +1200

    randomise the order of the answer options

--- quiz_query.py
+++ quiz_query.py
@@ -5,6 +5,7 @@ import random
 
 DATABASE = "quiz.db"
 ans_no = 0
+ans_list = []
 
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
@@ -12,6 +13,8 @@ query = "SELECT question FROM questions WHERE category_id = 4;"
 cursor.execute(query)
 results = cursor.fetchall()
 for question in results:
+    ans_list.clear()
+    ans_no = 0
     print("".join(question))
     id_query = "SELECT id FROM questions WHERE question = ?"
     cursor.execute(id_query, question)
@@ -19,14 +22,13 @@ for question in results:
     for item in q_id:
         for num in item:
             id = str(num)
+    cor_ans_query = "SELECT cor_answer FROM questions WHERE question = ?"
+    cursor.execute(cor_ans_query, (id,))
+    cor_ans = cursor.fetchall()
+    ans_list.append(cor_ans)
     ans_query = "SELECT wr_ans FROM ans_options WHERE question_id = ?"
     cursor.execute(ans_query, (id,))
     wr_ans = cursor.fetchall()
     for ans in wr_ans:
-        ans_no += 1
-        if ans_no == 1:
-            print(f"A) {"".join(ans)}")
-        if ans_no == 2:
-            print(f"B) {"".join(ans)}")
-        if ans_no == 3:
-            print(f"C) {"".join(ans)}")           
+        ans_list.append(ans)
+    random.shuffle(ans_list)     
