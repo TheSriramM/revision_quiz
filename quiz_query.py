c8c962f9b05b3d91e84409bf4b110fde6479c743
TheSriramM <161264109+TheSriramM@users.noreply.github.com>
2025-05-13 09:24:41 +1200

    receives input

--- quiz_query.py
+++ quiz_query.py
@@ -6,7 +6,11 @@ import random
 DATABASE = "quiz.db"
 ans_no = 0
 ans_list = []
+score = 0
 
+print("Welcome to the Y11 revision database!")
+# For next time
+# category = input("What subject do you want to revise (maths, science or english): ")
 db = sqlite3.connect(DATABASE)
 cursor = db.cursor()
 query = "SELECT question FROM questions WHERE category_id = 4;"
@@ -22,13 +26,44 @@ for question in results:
     for item in q_id:
         for num in item:
             id = str(num)
-    cor_ans_query = "SELECT cor_answer FROM questions WHERE question = ?"
-    cursor.execute(cor_ans_query, (id,))
+    cor_ans_query = "SELECT cor_answer FROM questions WHERE id = ?"
+    cursor.execute(cor_ans_query, (int(id),))
     cor_ans = cursor.fetchall()
+    for tup in cor_ans:
+        cor_ans = "".join(tup)
     ans_list.append(cor_ans)
     ans_query = "SELECT wr_ans FROM ans_options WHERE question_id = ?"
     cursor.execute(ans_query, (id,))
     wr_ans = cursor.fetchall()
     for ans in wr_ans:
-        ans_list.append(ans)
-    random.shuffle(ans_list)     
+        ans_list.append("".join(ans))
+    random.shuffle(ans_list)
+    for item in ans_list:
+        ans_no += 1
+        if ans_no == 1:
+            print(f"A) {item}")
+            if cor_ans == item:
+                letter = "A"
+        elif ans_no == 2:
+            print(f"B) {item}")
+            if cor_ans == item:
+                letter = "B"
+        elif ans_no == 3:
+            print(f"C) {item}")
+            if cor_ans == item:
+                letter = "C"
+        elif ans_no == 4:
+            print(f"D) {item}")
+            if cor_ans == item:
+                letter = "D"
+    inp = input("Please enter A, B, C or D: ")
+    inp = inp.upper()
+    if inp == letter:
+        print("CORRECT!")
+        score += 1
+    else:
+        print("WRONG!")
+        print(f"The correct answer was {letter} - {cor_ans}")
+
+print(f"You got {score} answers correct out of 10.")
+db.close()
