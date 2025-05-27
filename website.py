a9a06fab81a2c7a712a3c05e23f4cc6e190096e3
TheSriramM <161264109+TheSriramM@users.noreply.github.com>
2025-05-27 21:37:54 +1200

    part of the website works

--- website.py
+++ website.py
@@ -13,8 +13,9 @@ app.secret_key = "my_secret_key"
 def db_connect():
     # Connecting to the database
     db = sqlite3.connect(DATABASE)
-    cursor = db.cursor()
-    return cursor
+    # Gets the data in a row format which behaves like a tuple and a dictionary
+    db.row_factory = sqlite3.Row
+    return db.cursor()
 
 @app.route('/')
 def index():
@@ -26,7 +27,7 @@ def index():
     cursor.close()
     return render_template('main.html', categories=categories)
 
-@app.route('/quiz/<int:subject_id>')
+@app.route('/quiz/<int:sub_id>')
 def quiz(sub_id):
     cursor = db_connect()
     # Query for getting the questions from the category selected by the user
@@ -37,48 +38,40 @@ def quiz(sub_id):
     session["cur_question_index"] = 0
     session['questions'] = [dict(q) for q in questions]
     cursor.close()
-    return redirect(url_for('question'))
+    return redirect(url_for('show_questions', sub_id=sub_id))
 
-@app.route('/questions')
-def questions(sub_id):
+@app.route('/questions/<int:sub_id>')
+# Function to show the questions for the selected subject
+def show_questions(sub_id):
     cursor = db_connect()
     cur_question_index = session.get("cur_question_index", 0)
     questions = session.get("questions")
     question = questions[cur_question_index]
     query = "SELECT * FROM ans_options WHERE question_id = ?"
-    ans_options = cursor.execute(query, question['id']).fetchall()
+    ans_options = cursor.execute(query, (question['id'],)).fetchall()
     options_text = []
     for option in ans_options:
         options_text.append(option)
     cursor.close()
+    return render_template('question.html', question=question, options=options_text)
+
+@app.route('/answer', methods=['POST'])
+def answer():
+    question_id = request.form['question_id']
+    selected_option = request.form['selected_option']
+    cursor = db_connect()
+    query = "SELECT * FROM ans_options WHERE id > 210 AND question_id = ?"
+    cursor.execute(query, (question_id,))
+    questions = cursor.fetchall()
+    # Check if the selected option is correct  
+    correct = selected_option == questions[0]['answer']
+    session['cur_question_index'] += 1
+    cursor.close()
+    return render_template('answer.html', correct=correct, question=questions)
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# def math_questions():
-#     query = "SELECT question FROM questions WHERE category_id = 4 OR category_id = 5;"
-#     cursor.execute(query)
-#     results = cursor.fetchall()
-#     random.shuffle(results)
-#     for question in results:
-#         print("".join(question))
-
-# def choices(question):
-#     id = "SELECT ID FROM questions WHERE question IS ?;"
-#     cursor.execute(id, question)
-#     results = cursor.fetchall()
-#     for item in results:
-#         string_id = "".join(results)
-#     cor_ans_query = "SELECT cor_ans FROM questions WHERE question IS ?;"
-#     cursor.execute(cor_ans_query, question)
-#     results = cursor.fetchall()
-#     for item in results:
-#         questions["cor_ans"] = "".join(item)
-#     ans_query = "SELECT wr_ans FROM ans_options WHERE question_id = ?"
-#     cursor.execute(ans_query, id)
-#     results = cursor.fetchall()
-#     ans_options = []
-#     for item in results:
-#         ans_options.append("".join(item))
-#     questions["answers"] = ans_options
